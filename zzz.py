05b2d5f32190a4b442498243f25c5cac44ebdc99
Christian DeWolf <me@christiandewolf.com>
2026-05-06 15:56:03 -0300

    Minor improvements

--- zzz.py
+++ zzz.py
@@ -13,19 +13,20 @@ right_sound   = mixer.Sound(sound_dir + "ding.mp3")
 # replace with your mouse's ID
 mouse = InputDevice('/dev/input/by-id/usb-HID-compliant_Mouse_HID-compliant_Mouse-event-mouse')
 
-print(formatted_time, "Ready! Listening for input...")
+def formatted_time():
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+print(formatted_time(), "Ready! Listening for input...")
 ready_sound.play()
 
 for event in mouse.read_loop():
     if event.type == ecodes.EV_KEY:
-        formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-
         if event.code == ecodes.BTN_LEFT and event.value == 1:
             trigger_sound.play()
-            print(formatted_time, "Trigger clicked!")
+            print(formatted_time(), "Trigger clicked!")
         elif event.code == ecodes.BTN_RIGHT and event.value == 1:
             right_sound.play()
-            print(formatted_time, "Right button clicked!")
+            print(formatted_time(), "Right button clicked!")
         elif event.code == ecodes.BTN_MIDDLE and event.value == 1:
             left_sound.play()
-            print(formatted_time, "Left button clicked!")
+            print(formatted_time(), "Left button clicked!")
